e3b8fe717e8e26a83b51ff3fba60a4321b939f56
Lev <108870380+Levandriy@users.noreply.github.com>
2022-12-17 14:21:09 +0300

    0.0.1
    Test

---
+++ Test/Models/Access_cons.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend_ASPNET.Models
+{
+    public class Access_cons
+    {
+        public int Id { get; set; }
+        public int Material_id { get; set; } // имя пользователя
+        public int Access_id { get; set; }
+    }
+}
---
+++ Test/Models/Materials.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend_ASPNET.Models
+{
+    public class Materials
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; } 
+        public string? File { get; set; }
+    }
+}
